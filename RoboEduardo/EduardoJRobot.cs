1df9782f607e6c5ae35ec438b4dd463336417796
Eduardo <edujcordeiro99@gmail.com>
2018-06-07 22:09:19 -0300

    Configurando robo
    Configurando robo

---
+++ RoboEduardo/EduardoJRobot.cs
@@ -0,0 +1,26 @@
+﻿using Robocode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboEduardo
+{
+    public class EduardoJRobot : Robot
+    {
+        public override void Run()
+        {
+            
+
+            while(true)
+            {
+                Ahead(100);
+                TurnLeft(40);
+                Fire(3);
+                
+            }
+        }
+        
+    }
+}
